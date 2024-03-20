e3f067d0e451586a1a03a2252cd4640711789993
Foshati <amirrezafoshati@gmail.com>
2024-03-20 11:49:03 +0330

    start project catalogfa with react,bun,tailwindcss and create header components

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,12 @@
+import "../Header/header.css";
+function Header() {
+  return (
+    <div className="header rounded-xl m-4">
+      <h2 className="text-3xl m-6 text-red-600 font-bold">
+        Fast food catalogue
+      </h2>
+    </div>
+  );
+}
+
+export default Header;
