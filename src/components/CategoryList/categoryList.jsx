784080f69b41107142c7398909cbbf090f4cfcda
Foshati <amirrezafoshati@gmail.com>
2024-03-24 22:46:04 +0330

    fix header

--- src/components/CategoryList/categoryList.jsx
+++ src/components/CategoryList/categoryList.jsx
@@ -1,16 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "../../axios";
 
 const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     const fetchCategories = async () => {
       const response = await axios.get("/FoodCategory/categories");
-      console.log(response.data);
+      setCategories(response.data);
     };
     fetchCategories();
   }, []);
 
-  return <div> ali</div>;
+  return (
+    <nav className="max-w-6xl p-4 mx-auto -mt-8 bg-red-200 navbar rounded-xl">
+      <div>
+        <ul className="flex flex-row-reverse justify-around">
+          <a href="">همه محصولات</a>
+
+          {categories.map((category) => (
+            <li key={category.id}>
+              <a>{category.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default CategoryList;
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,7 @@
 import "../Header/header.css";
 function Header() {
   return (
-    <div className="m-4 header rounded-xl">
+    <div className="mt-4 header rounded-xl">
       <h2 className="m-8 text-xl font-bold text-red-600">
         Fast food catalogue
       </h2>
