290dea33546873d7eb9a03fdc5e31b877e9bb8a3
Foshati <amirrezafoshati@gmail.com>
2024-04-10 20:45:41 +0330

    add framer-motion and add animation linear in horiz items

--- src/components/FastFoodList/fastFoodList.jsx
+++ src/components/FastFoodList/fastFoodList.jsx
@@ -1,11 +1,21 @@
+import { motion } from "framer-motion";
 import { FastFoodItem } from "../FastFoodItem/fastFoodItem";
 
 const FastFoodLists = ({ fastFoodItems }) => (
-  <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
-    {fastFoodItems.map((fastFoodItem) => (
-      <div key={fastFoodItem.id} className="flex justify-center">
+  <div
+    className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3"
+    style={{ direction: "rtl" }}
+  >
+    {fastFoodItems.map((fastFoodItem, index) => (
+      <motion.div
+        key={fastFoodItem.id}
+        className="flex justify-center "
+        initial={{ opacity: 0, x: -25 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: index * 0.12, type: "linear", duration: 0.8 }}
+      >
         <FastFoodItem {...fastFoodItem} />
-      </div>
+      </motion.div>
     ))}
   </div>
 );
